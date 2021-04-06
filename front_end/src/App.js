1491fef7e1823302fc1bc81ea23512debb1c2f4d
shatruk <you@example.com>
2021-04-06 23:22:28 +0300

    -Сделана начальная структура проекта

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+/*React*/
+import React from 'react'
+/*Libraries*/
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    let div = <>
+        <div className="App">
+            <Container fluid={true}>
+                <Row>
+                    <Col>
+                        {/*Сдесь будет хедер*/}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        {/*Сдесь будет навигация*/}
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolorum impedit
+                        inventore ipsum officia perferendis similique sunt vero? Autem consectetur corporis dolore
+                        doloremque ipsum laudantium nemo saepe sunt tempore vitae?
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Row>
+                    <Col sm={8}>{/*Сдесь контент*/}
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab commodi deserunt dolor
+                        doloribus earum eos est fuga, nostrum omnis optio perferendis porro quae
+                        quibusdam quo quod, sapiente temporibus voluptatum. Quia.
+                    </Col>
+                    <Col sm={4}>{/*Место для популярных статей*/}
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab commodi deserunt dolor
+                        doloribus earum eos est fuga, nostrum omnis optio perferendis porro quae
+                        quibusdam quo quod, sapiente temporibus voluptatum. Quia.
+                    </Col>
+                </Row>
+            </Container>
+            <Container>
+                <Row>
+                    <Col>
+                        {/*footer*/}
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab commodi deserunt dolor
+                        doloribus earum eos est fuga, nostrum omnis optio perferendis porro quae
+                        quibusdam quo quod, sapiente temporibus voluptatum. Quia.
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    </>;
+    return div;
 }
 
 export default App;
