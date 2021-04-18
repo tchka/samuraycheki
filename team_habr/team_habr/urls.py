16f51107f9e69c99fa10e34b6653263df226778b
shatruk <you@example.com>
2021-04-18 19:54:16 +0300

    -Добавил автодокументацию доступна по адрессу http://<base_url>/swagger/

--- team_habr/team_habr/urls.py
+++ team_habr/team_habr/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .yasg import urlpatterns as doc_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,8 @@ urlpatterns = [
 
     path('api/', include('main_app.urls')),
 ]
+
+urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
